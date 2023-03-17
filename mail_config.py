101d1e1515a8d879eb8226baacf553ad204e262e
divyanshsaxena11 <divyanshsaxenaofficial@gmail.com>
2023-03-17 12:26:51 +0530

    new file:   mail_config.py

---
+++ mail_config.py
@@ -0,0 +1,12 @@
+# %%
+import email.message, smtplib, re
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
+
+# %%
+sender_onBehalf = "XXXXXXXX@gmail.com"
+login_mail = "XXXXXXXX@gmail.com"
+login_credential = "XXXXXXXX"
+
+
